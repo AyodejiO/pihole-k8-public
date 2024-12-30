cba1ee8100dc61c8a370543e5755dc2dc8dc6695
OG Sam <sam93.od@gmail.com>
2024-12-30 04:51:10 +0000

    fix: outOfSync

--- install-k8-adlists-list.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-kubectl create configmap pihole-adlists-list -n pihole --from-file=adlists.list
--- install-k8-custom-list.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# the custom list has the local dns entries
-# this will copy the file into a k8 configmap entry
-kubectl create configmap pihole-custom-list -n pihole --from-file=custom.list
